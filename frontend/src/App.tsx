f7ef86c711b17f2427c524cc3f3259d883aba063
John Ballesteros <johnshift.dev@gmail.com>
2021-10-01 10:19:10 +0800

    frontend to vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,54 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import './App.css';
 
-// components
-import Navbar from './components/navbar/Navbar';
+import React, { useState } from 'react';
 
-// pages
-import Home from './pages/Home';
-import Link1 from './pages/Link1';
-import Link2 from './pages/Link2';
-import Link3 from './pages/Link3';
-import NotFound from './pages/NotFound';
+import logo from './logo.svg';
 
-const App = (): JSX.Element => (
-  <div className="App">
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/link-1">
-          <Link1 />
-        </Route>
-        <Route path="/link-2">
-          <Link2 />
-        </Route>
-        <Route path="/link-3">
-          <Link3 />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
-  </div>
-);
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="header">
+          🚀 Vite + React + Typescript 🤘 & <br />
+          Eslint 🔥+ Prettier
+        </p>
+
+        <div className="body">
+          <button onClick={() => setCount((count) => count + 1)}>
+            🪂 Click me : {count}
+          </button>
+
+          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
+
+          <p>
+            Mess up the code in <code>App.tsx </code> and save the file.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {' | '}
+            <a
+              className="App-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+          </p>
+        </div>
+      </header>
+    </div>
+  );
+}
 
 export default App;
